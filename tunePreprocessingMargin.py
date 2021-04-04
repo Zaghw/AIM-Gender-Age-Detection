12bc01003ada9e54439fe75a08bd268f3b73b59a
Karim Zaghw <eag6akarimzaghw@gmail.com>
2021-04-04 15:50:54 +0200

    adjusted output message after tuning

--- tunePreprocessingMargin.py
+++ tunePreprocessingMargin.py
@@ -5,9 +5,11 @@ if __name__ == "__main__":
     bestMargin = -1
     bestValidCost = -1
 
-    for margin in range(0, 51, 1):
+    for margin in range(0, 51, 5):
         preprocessDataset(margin)
         validCost = trainModel()
         if validCost < bestValidCost or bestValidCost == -1:
             bestMargin = margin
             bestValidCost = validCost
+    print("Best Margin: ", str(bestMargin))
+    print("Best Validation Cost: ", str(bestValidCost))
