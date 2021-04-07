146a67d3edc1229a6d6140920fa082339a736449
Karim Zaghw <eag6akarimzaghw@gmail.com>
2021-04-07 20:54:54 +0200

    temp revert to see wtf is going on

--- IMDBWIKIDataset.py
+++ IMDBWIKIDataset.py
@@ -7,7 +7,7 @@ import torch
 class IMDBWIKIDataset(Dataset):
     """Custom Dataset for loading IMDB-WIKI face images"""
 
-    def __init__(self, csv_path, img_dir, NUM_AGE_CLASSES, MIN_AGE, transform=None):
+    def __init__(self, csv_path, img_dir, NUM_AGE_CLASSES, transform=None):
 
         df = pd.read_csv(csv_path, index_col=0)
         self.img_dir = img_dir
@@ -17,7 +17,6 @@ class IMDBWIKIDataset(Dataset):
         self.genders = df['genders'].values
         self.transform = transform
         self.NUM_AGE_CLASSES = NUM_AGE_CLASSES
-        self.MIN_AGE = MIN_AGE
 
     def __getitem__(self, index):
         img = Image.open(os.path.join(self.img_dir,
@@ -26,19 +25,18 @@ class IMDBWIKIDataset(Dataset):
         if self.transform is not None:
             img = self.transform(img)
 
-        age_label = self.ages[index]
-        age_class = age_label - self.MIN_AGE
+        age = self.ages[index]
         # Map age to one of the predefined calsses
         # TODO: make this part more automated and cleaner
-        # if age <= 24:
-        #     age_label = 0
-        # elif age <= 34:
-        #     age_label = 1
-        # elif age <= 49:
-        #     age_label = 2
-        # else:
-        #     age_label = 3
-        age_levels = [1]*age_class + [0]*(self.NUM_AGE_CLASSES - 1 - age_class)
+        if age <= 24:
+            age_label = 0
+        elif age <= 34:
+            age_label = 1
+        elif age <= 49:
+            age_label = 2
+        else:
+            age_label = 3
+        age_levels = [1]*age_label + [0]*(self.NUM_AGE_CLASSES - 1 - age_label)
         age_levels = torch.tensor(age_levels, dtype=torch.float32)
 
         gender_label = self.genders[index]
